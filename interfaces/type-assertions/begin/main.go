e5e5c329fd0e597405b81a492f024e609f740777
Rutvij Pandya <rutvij.pandya.2010@gmail.com>
2022-12-23 11:29:09 +0000

    ch5

--- interfaces/type-assertions/begin/main.go
+++ interfaces/type-assertions/begin/main.go
@@ -1,8 +1,16 @@
 // interfaces/type-assertions/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// perform a type assertion
+	var i any = "Hello"
+	// var i any = 10
+	// fmt.Println(i.(string))
 
 	// perform a type assertion and handle the error
+	if _, ok := i.(int); !ok {
+		fmt.Printf("%v is not an integer \n", i)
+	}
 }
